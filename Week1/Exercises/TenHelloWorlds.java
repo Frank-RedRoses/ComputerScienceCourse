54b0bc84bb4102b696500365dffd54bb81576772
Frank_theone <frank_theone@hotmail.com>
2022-09-27 16:49:11 +0200

    first commit

---
+++ Week1/Exercises/TenHelloWorlds.java
@@ -0,0 +1,21 @@
+package Week1.Exercises;
+/*
+ *  Write a program TenHelloWorlds.java that prints "Hello, World" ten times.
+ */
+
+public class TenHelloWorlds {
+
+  public static void main(String[] args) {
+    System.out.println("Hello, World");
+    System.out.println("Hello, World");
+    System.out.println("Hello, World");
+    System.out.println("Hello, World");
+    System.out.println("Hello, World");
+    System.out.println("Hello, World");
+    System.out.println("Hello, World");
+    System.out.println("Hello, World");
+    System.out.println("Hello, World");
+    System.out.println("Hello, World");
+    System.out.println("Hello, World");
+  }
+}
